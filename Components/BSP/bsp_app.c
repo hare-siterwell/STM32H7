b94f00a2ab312f3e7dd9f3472f5fc34af5575fce
Hare <renilr0@outlook.com>
2021-10-14 20:31:19 +0800

    .

--- Components/BSP/bsp_app.c
+++ Components/BSP/bsp_app.c
@@ -28,25 +28,23 @@ void app_task(void *p_arg) {
   DWT_Init();
   USART_Enable();
   SPI_Enable();
-  motor_arg_init();
   LL_TIM_EnableIT_UPDATE(TIM3);
   LL_TIM_EnableCounter(TIM3);
+  motor_arg_init();
 
-  // #if OS_CFG_SCHED_ROUND_ROBIN_EN
-  //   OSSchedRoundRobinCfg(DEF_ENABLED, 0, &err);
-  // #endif
+#if OS_CFG_SCHED_ROUND_ROBIN_EN
+  OSSchedRoundRobinCfg(DEF_ENABLED, 0, &err);
+#endif
 
   OSTaskCreate(&Task1TCB, "Task1", lpuart1_task, 0, 5, Task1Stk, 512 / 10, 512,
                0, 0, 0, OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR, &err);
 
-  // OSTaskCreate(&Task2TCB, "Task2", usart3_task, 0, 6, Task2Stk, 512 / 10,
-  // 512,
-  //              0, 0, 0, OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR, &err);
+  OSTaskCreate(&Task2TCB, "Task2", usart3_task, 0, 6, Task2Stk, 512 / 10, 512,
+               0, 0, 0, OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR, &err);
 
-  // OSTaskCreate(&Task3TCB, "Task3", lcd_task, 0, 7, Task3Stk, 512 / 10,
-  // 512, 0,
-  //              0, 0, OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR, &err);
+  OSTaskCreate(&Task3TCB, "Task3", lcd_task, 0, 7, Task3Stk, 512 / 10, 512, 0,
+               0, 0, OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR, &err);
 
-  OSTaskCreate(&Task4TCB, "Task4", led_task, 0, 8, Task4Stk, 512 / 10, 512, 0,
+  OSTaskCreate(&Task4TCB, "Task4", led_task, 0, 7, Task4Stk, 512 / 10, 512, 0,
                0, 0, OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR, &err);
 }
